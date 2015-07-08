d029796eb88a4730783a19c759ddbfd55979fd4a
OSK-Master <os_ken@ed.tus.ac.jp>
2015-07-08 21:20:27 +0900

    add main script

--- Prototype-2/main.py
+++ Prototype-2/main.py
@@ -1,8 +1,14 @@
 #! /usr/bin/python2
 
+import gp
+
 class Main:
     def __init__(self):
         print "this is genom program."
 
 if __name__ == "__main__":
     main = Main()
+    random1 = gp.makerandomtree(2)
+    random1.display()
+    hiddenset = gp.buildhiddenset()
+    print gp.scorefunction(random1,hiddenset)
