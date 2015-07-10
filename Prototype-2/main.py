dce1d18063b46cd01bfcf4cfe3de9cd6641d7767
ArLE <ARLEQUIN.prx@gmail.com>
2015-07-10 15:25:56 +0900

    fix elite and little bugs

--- Prototype-2/main.py
+++ Prototype-2/main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python2
 
 import gp
+import gridwar
 
 class Main:
     def __init__(self):
@@ -11,15 +12,19 @@ if __name__ == "__main__":
     '''
     rf = gp.getrankfunction(gp.buildhiddenset())
     gp.evolve(2, 500, rf, mutationrate = 0.2, breedingrate = 0.1,
-            pexp = 0.7, pnew = 0.1)
+            elite = 20, pnew = 0.1)
     '''
-    t_winner = gp.evolve(5, 100, gp.tournament, maxgen = 200,
+    t_winner = gp.evolve(5, 100, gridwar.tournament, maxgen = 100,
             mutationrate = 0.1, breedingrate = 0.3,
-            pexp = 0.2, pnew = 0.05)
-    winner = gp.gridgame([t_winner, gp.humanplayer()])
+            elite = 10, pnew = 0.05)
+    winner = gridwar.gridgame([t_winner, gridwar.humanplayer()])
     if winner == 0:
         print 'You lose.'
     elif winner == 1:
         print 'You win!'
+    elif winner == 2:
+        print 'KO! You lose.'
+    elif winner == 3:
+        print 'KO! You win!'
     else:
         print 'Draw'
